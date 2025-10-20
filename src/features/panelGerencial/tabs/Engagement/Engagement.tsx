33b77de06d649510e2d2e4efdf100c486f2edef7
stickfather <benstickley12@gmail.com>
2025-10-20 06:34:14 -0500

    Remove legacy management reports

--- src/features/panelGerencial/tabs/Engagement/Engagement.tsx
+++
@@ -1,5 +0,0 @@
-import { EngagementDashboard } from "@/components/management/engagement/EngagementDashboard";
-
-export default function EngagementPanel() {
-  return <EngagementDashboard initialLanguage="es" />;
-}
